57d600c108617a9b8e43b5dd477bbacc6f2cc5a4
dahlo <m.dahlo@gmail.com>
2024-12-06 09:41:00 +0100

    polish

--- api_reader.bad.py
+++ api_reader.bad.py
@@ -1,11 +1,11 @@
 import requests
-import yaml
+
+
+
 
 # Define the API endpoint
 api_url = 'https://example-api.com/data'
 api_key = 'SEUNiTMcpNyXKXnnaNTz70pe0kWumpKrK2vVd0NGL51h3XVDXtZumw0CfVJST12uP3k0vrJPrXgiokerpIgWkHWzRcOTyWIdBdASAdp5nEsShs2EH6HUbgCJEx4NNBW6'
-
-
 
 # Call the API with the Authorization header
 response = requests.get(api_url, headers={'Authorization': f'Bearer {api_key}'})
--- api_reader.better.py
+++ api_reader.better.py
@@ -4,8 +4,8 @@ import yaml
 # Read API key from the YAML file
 with open('config.yaml', 'r') as file:
     config = yaml.safe_load(file)
-api_key = config['api_key']
 api_url = config['api_url']
+api_key = config['api_key']
 
 # Call the API with the Authorization header
 response = requests.get(api_url, headers={'Authorization': f'Bearer {api_key}'})
@@ -13,5 +13,3 @@ data = response.json()
 
 # Print the response data
 print(data)
-
-# 
