29dd3ad094df6157fcce50955b6c9523842eeb33
dahlo <m.dahlo@gmail.com>
2024-12-06 10:47:55 +0100

    shortened api key

--- api_reader.bad.py
+++ api_reader.bad.py
@@ -5,7 +5,7 @@ import requests
 
 # Define the API endpoint
 api_url = 'https://example-api.com/data'
-api_key = 'SEUNiTMcpNyXKXnnaNTz70pe0kWumpKrK2vVd0NGL51h3XVDXtZumw0CfVJST12uP3k0vrJPrXgiokerpIgWkHWzRcOTyWIdBdASAdp5nEsShs2EH6HUbgCJEx4NNBW6'
+api_key = 'SEUNiTMcpNyXKXnnaNTz70pe0kWu'
 
 # Call the API with the Authorization header
 response = requests.get(api_url, headers={'Authorization': f'Bearer {api_key}'})
